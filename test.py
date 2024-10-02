13af9cc1b300f1d55142d58960d6e1cab7d98986
Elialeonardi02 <e.leonardi5@studenti.unipi.it>
2024-10-02 12:10:23 +0200

    remove pragma

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 #counts even numbers between two integers
 def fun(a:int, b:int)->int:
     c:int=0
-    #pragma HLS LATENCY min=1 max=2
     for i in range(a,b):
         if i%2==0:
             c+=1
