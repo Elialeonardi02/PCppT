c8872f29834149348f4948774d7342b64d0d9c9a
Elialeonardi02 <e.leonardi5@studenti.unipi.it>
2024-10-29 15:56:39 +0100

    lambda: +corretto return mancante

--- test/test.cpp
+++ test/test.cpp
@@ -10,11 +10,10 @@ void fun1()
 void fun(int y)
 {
   int z = 1;
-  print();
   for (int i = 0; i < 1; ++i) {
     z += 2;
   }
-  auto l = [](auto x, auto y) {x + y;};
+  auto l = [](auto x, auto y) {return z + y;};
   l(1, 2);
 }
 
--- test/test.py
+++ test/test.py
@@ -3,9 +3,8 @@ def fun1()->None:
     c=2
 def fun (y:int)->None:
     z=1
-    print()
     for i in range(0,1):
        z+=2
-    l = lambda x, y: x + y
+    l = lambda x, y: z + y
     l(1,2)
 
